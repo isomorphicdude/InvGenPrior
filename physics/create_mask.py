551c07ef09ff4db62853942e63bfd0eb02d56cc8
isomorphicdude <isometricat@icloud.com>
2024-07-01 11:23:23 +0800

    add mask

--- physics/create_mask.py
+++ physics/create_mask.py
@@ -53,3 +53,39 @@ def square_box(
     )
 
     return mask, missing_indices, kept_indices, coordinates_mask
+
+
+def random_pixel_mask(ratio_to_mask = 0.3,
+                      D_OR = (3, 256, 256),
+                      device='cpu'):
+    """
+    Creates a random pixel-wise mask for inpainting.
+    Args:
+        - ratio_to_mask: Float representing the ratio of pixels to mask; higher
+            values will mask more pixels.
+        - D_OR: Tuple of integers representing the shape of the image tensor (channels, height, width).
+        - device: String representing the device to use; default is 'cpu'.
+    
+    Returns:  
+        - mask: torch.tensor (height, width) representing the mask.
+        - missing_indices: torch.tensor (n,) length same as flattened missing part
+        - kept_indices: torch.tensor (n,) length same as flattened kept part
+        - coordinates_mask: torch.tensor (c*h*w, ), entries are boolean
+    """
+    channels, h, w = D_OR
+
+    mask = torch.rand(*D_OR[1:]) < ratio_to_mask
+
+    missing_r = torch.nonzero(mask.flatten()).long().reshape(-1) * 3
+    missing_g = missing_r + 1
+    missing_b = missing_g + 1
+    missing_indices = torch.cat([missing_r, missing_g, missing_b], dim=0)
+
+    kept_indices = torch.Tensor([i for i in range(channels * h * w) if i not in missing_indices]).to(device).long()
+
+    coordinates_mask = torch.isin(
+        torch.arange(math.prod(D_OR), device=device),
+        torch.arange(kept_indices.shape[0], device=device))
+
+
+    return mask, missing_indices, kept_indices, coordinates_mask
