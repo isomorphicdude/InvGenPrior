7e440f5c94258a54ae675d0e9b6ce18975ae63be
isomorphicdude <isometricat@icloud.com>
2024-07-07 20:12:40 +0800

    add to solve config issues

--- configs/tmpd/celeba/inpaint_box.py
+++ configs/tmpd/celeba/inpaint_box.py
@@ -1,11 +1,20 @@
 """Configuration for inpainting on CelebA using TMPD."""
+import os
+import sys
+
+project_dir = os.path.abspath(os.path.join(os.path.dirname(__file__),  '..', '..', '..'))
+config_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))
+
+# Add the project directory to the sys.path
+sys.path.append(project_dir)
+sys.path.append(config_dir)
 
 import numpy as np
 
 import ml_collections
 
 from physics.create_mask import load_mask
-from ...celeb_configs import get_config as get_celeb_config
+from celeb_configs import get_config as get_celeb_config
 
 def get_config():
     config = get_celeb_config()
