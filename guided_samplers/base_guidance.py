e6f4da7f7a5b4ae56181b993d496b07e0ff71c3d
isomorphicdude <isometricat@icloud.com>
2024-07-07 20:48:05 +0800

    fix sampler

--- guided_samplers/base_guidance.py
+++ guided_samplers/base_guidance.py
@@ -8,45 +8,6 @@ import torch
 # custom imports
 from models import utils as mutils
 
-__GUIDED_SAMPLERS__ = {}
-
-
-def register_guided_sampler(name: str):
-    def wrapper(cls):
-        if __GUIDED_SAMPLERS__.get(name, None):
-            raise NameError(f"Name {name} is already registered!")
-        __GUIDED_SAMPLERS__[name] = cls
-        return cls
-
-    return wrapper
-
-
-def get_guided_sampler(
-    name: str,
-    model,
-    sde,
-    shape,
-    sampling_eps,
-    inverse_scaler,
-    H_func,
-    noiser,
-    device,
-    **kwargs,
-) -> "GuidedSampler":
-    if __GUIDED_SAMPLERS__.get(name, None) is None:
-        raise NameError(f"Name {name} is not defined!")
-    return __GUIDED_SAMPLERS__[name](
-        model_fn=model,
-        sde=sde,
-        shape=shape,
-        sampling_eps=sampling_eps,
-        inverse_scaler=inverse_scaler,
-        H_func=H_func,
-        noiser=noiser,
-        device=device,
-        **kwargs,
-    )
-
 
 class GuidedSampler(ABC):
     """Base class for guided samplers."""
