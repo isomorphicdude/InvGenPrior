41f9d8f62448bbec71e0674f0a6443e533d0d8d3
isomorphicdude <isometricat@icloud.com>
2024-07-07 17:48:17 +0800

    solve import issue

--- data/convert_to_lmdb.py
+++ data/convert_to_lmdb.py
@@ -1,6 +1,13 @@
 """Convert a folder of images in the same directory to lmdb."""
 
 from absl import app, flags
+import sys
+import os
+
+
+script_dir = os.path.dirname(__file__)
+project_dir = os.path.join(script_dir, '..')
+sys.path.append(project_dir)
 
 from datasets import lmdb_dataset
 
@@ -8,6 +15,8 @@ from datasets import lmdb_dataset
 flags.DEFINE_string("dpath", None, "Path to the folder of images.")
 flags.DEFINE_string("split", "train", "Split of the dataset.")
 flags.DEFINE_integer("num_workers", 1, "Number of workers.")
+
+flags.mark_flags_as_required(["dpath"])
 
 FLAGS = flags.FLAGS
 
