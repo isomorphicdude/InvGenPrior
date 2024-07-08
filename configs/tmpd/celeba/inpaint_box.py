628aeaa1c1a95bf8f2065d8c1acd079f4070fd77
isomorphicdude <isometricat@icloud.com>
2024-07-08 12:50:53 +0800

    remove clipping

--- configs/tmpd/celeba/inpaint_box.py
+++ configs/tmpd/celeba/inpaint_box.py
@@ -46,7 +46,7 @@ def get_config():
     sampling = config.sampling
     sampling.gudiance_method = "tmpd"
     sampling.use_ode_sampler = "euler"
-    sampling.clamp_to = 1 # gradient clipping
+    sampling.clamp_to = None # gradient clipping
     sampling.batch_size = 5 
     sampling.sample_N = 10
     
