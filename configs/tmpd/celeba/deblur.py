e61f355750578028c47603b033ca1456d87dc915
isomorphicdude <isometricat@icloud.com>
2024-07-12 13:13:47 +0100

    add clipping and step no.

--- configs/tmpd/celeba/deblur.py
+++ configs/tmpd/celeba/deblur.py
@@ -57,9 +57,9 @@ def get_config():
     sampling = config.sampling
     sampling.gudiance_method = "tmpd"
     sampling.use_ode_sampler = "euler"
-    sampling.clamp_to = 10 # gradient clipping
+    sampling.clamp_to = 1 # gradient clipping
     sampling.batch_size = 2 
-    sampling.sample_N = 10
+    sampling.sample_N = 50
     
     return config
     
