8efd09807406b7bfd83245b8668e1032ee2b665b
isomorphicdude <isometricat@icloud.com>
2024-07-14 16:28:22 +0100

    remove imports

--- guided_samplers/pgdm.py
+++ guided_samplers/pgdm.py
@@ -1,8 +1,8 @@
-"""Implements the TMPD guidance functions."""
+"""Implements the PGDM guidance functions."""
 
 import math
 import torch
-import functorch
+
 from guided_samplers.base_guidance import GuidedSampler
 from guided_samplers.registry import register_guided_sampler
 from models.utils import convert_flow_to_x0
@@ -53,11 +53,11 @@ class PiGDM(GuidedSampler):
         grad_term = grad_term.detach()  / r_t_2
         
         scaled_grad = grad_term * (std_t**2) * (1 / alpha_t + 1 / std_t)
-        
-        # print(scaled_grad.mean())
          
         # compute gamma_t scaling
         gamma_t = math.sqrt(alpha_t / (alpha_t**2 + std_t**2))
+        
+        print((gamma_t * scaled_grad).mean())
         
         return gamma_t * scaled_grad + flow_pred
         
