6efa22fe8e65d4a0ab8e7577e362439665b97da6
isomorphicdude <isometricat@icloud.com>
2024-07-17 17:01:13 +0100

    add clamping for reddiff

--- configs/reddiff/afhq/inpaint_box.py
+++ configs/reddiff/afhq/inpaint_box.py
@@ -50,7 +50,7 @@ def get_config():
     # sampling.use_ode_sampler = "rk45"
     sampling.clamp_to = 1 # gradient clipping for the guidance
     sampling.batch_size = 2
-    sampling.sample_N = 100 # NOTE: tune this
+    sampling.sample_N = 500 # NOTE: tune this
     sampling.sigma_variance = 0.0 # NOTE: tune this add noise and denoise?
     # does flow models denoise? can it go off the data manifold?
     sampling.starting_time = 0
