d0cc7fe86fc1c860fa4099be138474baaa10fc97
isomorphicdude <isometricat@icloud.com>
2024-07-18 08:51:02 +0100

    add creation of H

--- physics/create_mask.py
+++ physics/create_mask.py
@@ -127,6 +127,32 @@ def load_mask(mask_path, device="cpu"):
 
     return mask, missing_indices, kept_indices, coordinates_mask
 
+def get_H_mat(dim, obs_dim):
+    """
+    Returns the H_mat, its V, U, and singulars (same shape as obs_dim).
+
+    Args:
+        - dim: int, dimension of the true data
+        - obs_dim: int, dimension of the observation space
+
+    Returns:
+        - H_mat: torch.Tensor, of shape (obs_dim, dim), the observation matrix
+        - U: torch.Tensor, of shape (dim, dim), the left singular vectors of H_mat
+        - S: torch.Tensor, of shape (dim,), the singular values of H_mat
+        - Vt: torch.Tensor, of shape (dim, obs_dim), the right singular vectors of H_mat
+    """
+    H_mat = torch.randn(obs_dim * dim).reshape(obs_dim, dim)
+    U, S, Vt = torch.linalg.svd(H_mat, full_matrices=True)
+    coordinate_mask = torch.ones_like(Vt[0])
+    coordinate_mask[len(S) :] = 0
+
+    # sampling Unif[0, 1] for the singular values
+    diag = torch.sort(torch.rand_like(S), descending=True).values
+
+    H_mat = U @ (torch.diag(diag)) @ Vt[coordinate_mask == 1, :]
+
+    return H_mat, U, diag, Vt[coordinate_mask == 1, :].T
+
 
 def main():
     """
