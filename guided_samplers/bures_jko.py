8b44e6ccf850600f44b3a932a3e0200735084d5c
isomorphicdude <isometricat@icloud.com>
2024-07-21 12:17:17 +0100

    refactor

--- guided_samplers/bures_jko.py
+++ guided_samplers/bures_jko.py
@@ -62,11 +62,11 @@ class BuresJKO(GuidedSampler):
 
             # get designated time steps
             # tau_1, ..., tau_n to target for divide-and-conquer
-            no_taus = 10
+            no_taus = 5
             taus_list = range(0, self.sde.sample_N, self.sde.sample_N // no_taus)
             
             # steps_per_tau = self.sde.sample_N // no_taus
-            steps_per_tau = 20
+            steps_per_tau = 100
 
             # for i in range(self.sde.sample_N):
             for tau in taus_list:
