bd9ba39f3d5b1743c6eec8014ceafe86908d7fe8
isomorphicdude <isometricat@icloud.com>
2024-07-27 20:23:26 +0100

    add clamping

--- configs/celeb_configs.py
+++ configs/celeb_configs.py
@@ -49,7 +49,7 @@ def get_config():
     
     # number of steps to run the sampler
     sampling.sample_N = 50 # NOTE: tune this
-    sampling.clamp_to = None # gradient clipping for the guidance
+    sampling.clamp_to = 1.0 # gradient clipping for the guidance
     
     # inverse problem settings
     sampling.degredation_sigma = 0.0
