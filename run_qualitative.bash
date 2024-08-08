eda70a7a51bddaaae53415125d996642e788a22c
isomorphicdude <isometricat@icloud.com>
2024-08-08 18:21:44 +0100

    update num sample_N

--- run_qualitative.bash
+++ run_qualitative.bash
@@ -10,28 +10,29 @@
 
 sigma_list=(0.0 0.01 0.05)
 # sigma_list=(0.05)
+sample_N=200
 
 for sigma in ${sigma_list[@]}; do
     echo "Running qualitative evaluation for sigma = $sigma for AFHQ dataset"
     # inpaint box
-    python run_qualitative.py --config /content/InvGenPrior/configs/tmpd/afhq/inpaint_box.py --workdir /content/InvGenPrior/ --sample_N 100 --noise_sigma $sigma --data_index 322
+    python run_qualitative.py --config /content/InvGenPrior/configs/tmpd/afhq/inpaint_box.py --workdir /content/InvGenPrior/ --sample_N $sample_N --noise_sigma $sigma --data_index 322
     # inpaint pixel
-    python run_qualitative.py --config /content/InvGenPrior/configs/tmpd/afhq/inpaint_pixel.py --workdir /content/InvGenPrior/ --sample_N 100 --noise_sigma $sigma --data_index 53
+    python run_qualitative.py --config /content/InvGenPrior/configs/tmpd/afhq/inpaint_pixel.py --workdir /content/InvGenPrior/ --sample_N $sample_N --noise_sigma $sigma --data_index 53
     # deblur
-    python run_qualitative.py --config /content/InvGenPrior/configs/tmpd/afhq/deblur.py --workdir /content/InvGenPrior/ --sample_N 100 --noise_sigma $sigma --data_index 2
+    python run_qualitative.py --config /content/InvGenPrior/configs/tmpd/afhq/deblur.py --workdir /content/InvGenPrior/ --sample_N $sample_N --noise_sigma $sigma --data_index 2
     # super-res
-    python run_qualitative.py --config /content/InvGenPrior/configs/tmpd/afhq/super_res.py --workdir /content/InvGenPrior/ --sample_N 100 --noise_sigma $sigma --data_index 15
+    python run_qualitative.py --config /content/InvGenPrior/configs/tmpd/afhq/super_res.py --workdir /content/InvGenPrior/ --sample_N $sample_N --noise_sigma $sigma --data_index 15
 done
 
 
 for sigma in ${sigma_list[@]}; do
     echo "Running qualitative evaluation for sigma = $sigma for CelebA dataset"
     # inpaint box
-    python run_qualitative.py --config /content/InvGenPrior/configs/tmpd/celeba/inpaint_box.py --workdir /content/InvGenPrior/ --sample_N 100 --noise_sigma $sigma --data_index 322
+    python run_qualitative.py --config /content/InvGenPrior/configs/tmpd/celeba/inpaint_box.py --workdir /content/InvGenPrior/ --sample_N $sample_N --noise_sigma $sigma --data_index 322
     # inpaint pixel
-    python run_qualitative.py --config /content/InvGenPrior/configs/tmpd/celeba/inpaint_pixel.py --workdir /content/InvGenPrior/ --sample_N 100 --noise_sigma $sigma --data_index 53
+    python run_qualitative.py --config /content/InvGenPrior/configs/tmpd/celeba/inpaint_pixel.py --workdir /content/InvGenPrior/ --sample_N $sample_N --noise_sigma $sigma --data_index 53
     # deblur
-    python run_qualitative.py --config /content/InvGenPrior/configs/tmpd/celeba/deblur.py --workdir /content/InvGenPrior/ --sample_N 100 --noise_sigma $sigma --data_index 2
+    python run_qualitative.py --config /content/InvGenPrior/configs/tmpd/celeba/deblur.py --workdir /content/InvGenPrior/ --sample_N $sample_N --noise_sigma $sigma --data_index 2
     # super-res
-    python run_qualitative.py --config /content/InvGenPrior/configs/tmpd/celeba/super_res.py --workdir /content/InvGenPrior/ --sample_N 100 --noise_sigma $sigma --data_index 15
+    python run_qualitative.py --config /content/InvGenPrior/configs/tmpd/celeba/super_res.py --workdir /content/InvGenPrior/ --sample_N $sample_N --noise_sigma $sigma --data_index 15
 done
