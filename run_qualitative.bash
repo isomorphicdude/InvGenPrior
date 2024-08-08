f7d95fd4d3a2df4cd63f5f179523cd1e0ce8230f
isomorphicdude <isometricat@icloud.com>
2024-08-08 17:13:04 +0100

    add bash script

---
+++ run_qualitative.bash
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Run the qualitative evaluation
+# sigma 0.0, 0.01, 0.05
+# sample_N 50, 100
+# inpaint box 52
+# inpaint pixel 53
+# deblur 2
+# super-res 15
+
+sigma_list=(0.0 0.01 0.05)
+# sigma_list=(0.05)
+
+for sigma in ${sigma_list[@]}; do
+    echo "Running qualitative evaluation for sigma = $sigma"
+    # inpaint box
+    python run_qualitative.py --config /content/InvGenPrior/configs/tmpd/afhq/inpaint_box.py --workdir /content/InvGenPrior/ --sample_N 100 --noise_sigma $sigma --data_index 322
+    # inpaint pixel
+    python run_qualitative.py --config /content/InvGenPrior/configs/tmpd/afhq/inpaint_pixel.py --workdir /content/InvGenPrior/ --sample_N 100 --noise_sigma $sigma --data_index 53
+    # deblur
+    python run_qualitative.py --config /content/InvGenPrior/configs/tmpd/afhq/deblur.py --workdir /content/InvGenPrior/ --sample_N 100 --noise_sigma $sigma --data_index 2
+    # super-res
+    python run_qualitative.py --config /content/InvGenPrior/configs/tmpd/afhq/super_res.py --workdir /content/InvGenPrior/ --sample_N 100 --noise_sigma $sigma --data_index 15
+done
