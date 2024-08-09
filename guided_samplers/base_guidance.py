e8d3436fbaa67281324043ee4841b9ec60755008
isomorphicdude <isometricat@icloud.com>
2024-08-09 09:01:54 +0100

    update clamp rule

--- guided_samplers/base_guidance.py
+++ guided_samplers/base_guidance.py
@@ -119,7 +119,7 @@ class GuidedSampler(ABC):
 
                 else:
                     if (
-                        i <=1
+                        i <=0
                         and self.__class__.__name__ != "REDdiff"
                     ):
                         clamp_condition = True
