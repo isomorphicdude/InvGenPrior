43268ee616443e15bb1717b386e6a8a1f641c952
isomorphicdude <isometricat@icloud.com>
2024-08-14 17:29:28 +0100

    change clamping rule

--- guided_samplers/dps.py
+++ guided_samplers/dps.py
@@ -64,8 +64,8 @@ class DPS(GuidedSampler):
         
         if clamp_to is not None and clamp_condition:
             # clamp_to = flow_pred.flatten().abs().max().item()
-            # return (dps_scaling_const * corrected_grad).clamp(-clamp_to, clamp_to) + flow_pred
-            return (dps_scaling_const * corrected_grad + flow_pred).clamp(-clamp_to, clamp_to)
+            return (dps_scaling_const * corrected_grad).clamp(-clamp_to, clamp_to) + flow_pred
+            # return (dps_scaling_const * corrected_grad + flow_pred).clamp(-clamp_to, clamp_to)
         else:
             return (dps_scaling_const * corrected_grad) + flow_pred
         
--- guided_samplers/tmpd.py
+++ guided_samplers/tmpd.py
@@ -149,7 +149,8 @@ class TMPD(GuidedSampler):
         if clamp_to is not None and clamp_condition:
             # clamp_to = flow_pred.flatten().abs().max().item()
             # guided_vec = (scaled_grad).clamp(-clamp_to, clamp_to) + (flow_pred)
-            guided_vec = (scaled_grad + flow_pred).clamp(-clamp_to, clamp_to)
+            # guided_vec = (scaled_grad + flow_pred).clamp(-clamp_to, clamp_to)
+            guided_vec = (scaled_grad).clamp(-clamp_to, clamp_to) + (flow_pred)
 
             # re-normalisation?
             # flow_pred_norm = torch.linalg.vector_norm(flow_pred, dim=1)
