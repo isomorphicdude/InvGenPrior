39a568cf0a7e15f6675155c4693de8e9aedfa100
isomorphicdude <isometricat@icloud.com>
2024-08-16 22:26:42 +0100

    refactor

--- guided_samplers/tmpd.py
+++ guided_samplers/tmpd.py
@@ -145,12 +145,10 @@ class TMPD(GuidedSampler):
         # clamp to interval
         if clamp_to is not None and clamp_condition:
             # clamp_to = flow_pred.flatten().abs().max().item()
-            if num_t < 0.1:
-                guided_vec = (scaled_grad).clamp(-clamp_to, clamp_to) + (flow_pred)
-            # if num_t < 0.2:
-            #     guided_vec = (scaled_grad + flow_pred).clamp(-clamp_to, clamp_to)
-            # else:
-            #     guided_vec = scaled_grad + flow_pred
+            if num_t < 0.2:
+                guided_vec = (scaled_grad + flow_pred).clamp(-clamp_to, clamp_to)
+            else:
+                guided_vec = scaled_grad + flow_pred
             # guided_vec = (scaled_grad + flow_pred)
             # re-normalisation?
             # flow_pred_norm = torch.linalg.vector_norm(flow_pred, dim=1)
