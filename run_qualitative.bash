9a3ac90288e0dbc55c02dcf16415bcb5d54319f2
isomorphicdude <isometricat@icloud.com>
2024-08-25 16:40:45 +0100

    update to run noisy experiment

--- run_qualitative.bash
+++ run_qualitative.bash
@@ -9,7 +9,7 @@
 # super-res 15
 
 # sigma_list=(0.0 0.01 0.05)
-sigma_list=(0.01)
+sigma_list=(0.01 1.0)
 sample_N=100
 sample_count=10
 
@@ -19,55 +19,55 @@ afhq_max_index=499
 celeba_min_index=0
 celeba_max_index=1999
 
-for ((i=0;i<sample_count;i++)); do
-    random_index=$((RANDOM % (afhq_max_index - afhq_min_index + 1) + afhq_min_index))
-    echo "Running qualitative evaluation for sigma = 0.01 for AFHQ dataset"
-    echo "Random index: $random_index"
-    # super-res
-    python run_qualitative.py --config /content/InvGenPrior/configs/tmpd/afhq/super_res.py --workdir /content/InvGenPrior/ --sample_N 100 --data_index "$random_index" --sampling_var 0.01 --clamp_to 1.0 --noise_sigma 0.01 --max_iter 5
-    # deblur
-    python run_qualitative.py --config /content/InvGenPrior/configs/tmpd/afhq/deblur.py --workdir /content/InvGenPrior/ --sample_N 100 --data_index "$random_index" --sampling_var 0.01 --clamp_to 1.0 --noise_sigma 0.01 --max_iter 5
-    # inpaint pixel
-    python run_qualitative.py --config /content/InvGenPrior/configs/tmpd/afhq/inpaint_pixel.py --workdir /content/InvGenPrior/ --sample_N 100 --data_index "$random_index" --sampling_var 0.01 --clamp_to 1.0 --noise_sigma 0.01 --max_iter 5
-    # inpaint box
-    python run_qualitative.py --config /content/InvGenPrior/configs/tmpd/afhq/inpaint_box.py --workdir /content/InvGenPrior/ --sample_N 100 --data_index "$random_index" --sampling_var 0.01 --clamp_to 1.0 --noise_sigma 0.01 --max_iter 5
-done
+# for ((i=0;i<sample_count;i++)); do
+#     random_index=$((RANDOM % (afhq_max_index - afhq_min_index + 1) + afhq_min_index))
+#     echo "Running qualitative evaluation for sigma = 0.01 for AFHQ dataset"
+#     echo "Random index: $random_index"
+#     # super-res
+#     python run_qualitative.py --config /content/InvGenPrior/configs/tmpd/afhq/super_res.py --workdir /content/InvGenPrior/ --sample_N 100 --data_index "$random_index" --sampling_var 0.01 --clamp_to 1.0 --noise_sigma 0.01 --max_iter 5
+#     # deblur
+#     python run_qualitative.py --config /content/InvGenPrior/configs/tmpd/afhq/deblur.py --workdir /content/InvGenPrior/ --sample_N 100 --data_index "$random_index" --sampling_var 0.01 --clamp_to 1.0 --noise_sigma 0.01 --max_iter 5
+#     # inpaint pixel
+#     python run_qualitative.py --config /content/InvGenPrior/configs/tmpd/afhq/inpaint_pixel.py --workdir /content/InvGenPrior/ --sample_N 100 --data_index "$random_index" --sampling_var 0.01 --clamp_to 1.0 --noise_sigma 0.01 --max_iter 5
+#     # inpaint box
+#     python run_qualitative.py --config /content/InvGenPrior/configs/tmpd/afhq/inpaint_box.py --workdir /content/InvGenPrior/ --sample_N 100 --data_index "$random_index" --sampling_var 0.01 --clamp_to 1.0 --noise_sigma 0.01 --max_iter 5
+# done
 
-for ((i=0;i<sample_count;i++)); do
-    random_index=$((RANDOM % (celeba_max_index - celeba_min_index + 1) + celeba_min_index))
-    echo "Running qualitative evaluation for sigma = 0.01 for CelebA dataset"
-    echo "Random index: $random_index"
-    # super-res
-    python run_qualitative.py --config /content/InvGenPrior/configs/tmpd/celeba/super_res.py --workdir /content/InvGenPrior/ --sample_N 100 --data_index "$random_index" --sampling_var 0.01 --clamp_to 1.0 --noise_sigma 0.01 --max_iter 5
-    # deblur
-    python run_qualitative.py --config /content/InvGenPrior/configs/tmpd/celeba/deblur.py --workdir /content/InvGenPrior/ --sample_N 100 --data_index "$random_index" --sampling_var 0.01 --clamp_to 1.0 --noise_sigma 0.01 --max_iter 5
-    # inpaint pixel
-    python run_qualitative.py --config /content/InvGenPrior/configs/tmpd/celeba/inpaint_pixel.py --workdir /content/InvGenPrior/ --sample_N 100 --data_index "$random_index" --sampling_var 0.01 --clamp_to 1.0 --noise_sigma 0.01 --max_iter 5
-    # inpaint box
-    python run_qualitative.py --config /content/InvGenPrior/configs/tmpd/celeba/inpaint_box.py --workdir /content/InvGenPrior/ --sample_N 100 --data_index "$random_index" --sampling_var 0.01 --clamp_to 1.0 --noise_sigma 0.01 --max_iter 5
-done
+# for ((i=0;i<sample_count;i++)); do
+#     random_index=$((RANDOM % (celeba_max_index - celeba_min_index + 1) + celeba_min_index))
+#     echo "Running qualitative evaluation for sigma = 0.01 for CelebA dataset"
+#     echo "Random index: $random_index"
+#     # super-res
+#     python run_qualitative.py --config /content/InvGenPrior/configs/tmpd/celeba/super_res.py --workdir /content/InvGenPrior/ --sample_N 100 --data_index "$random_index" --sampling_var 0.01 --clamp_to 1.0 --noise_sigma 0.01 --max_iter 5
+#     # deblur
+#     python run_qualitative.py --config /content/InvGenPrior/configs/tmpd/celeba/deblur.py --workdir /content/InvGenPrior/ --sample_N 100 --data_index "$random_index" --sampling_var 0.01 --clamp_to 1.0 --noise_sigma 0.01 --max_iter 5
+#     # inpaint pixel
+#     python run_qualitative.py --config /content/InvGenPrior/configs/tmpd/celeba/inpaint_pixel.py --workdir /content/InvGenPrior/ --sample_N 100 --data_index "$random_index" --sampling_var 0.01 --clamp_to 1.0 --noise_sigma 0.01 --max_iter 5
+#     # inpaint box
+#     python run_qualitative.py --config /content/InvGenPrior/configs/tmpd/celeba/inpaint_box.py --workdir /content/InvGenPrior/ --sample_N 100 --data_index "$random_index" --sampling_var 0.01 --clamp_to 1.0 --noise_sigma 0.01 --max_iter 5
+# done
 
 # for sigma in ${sigma_list[@]}; do
 #     echo "Running qualitative evaluation for sigma = $sigma for AFHQ dataset"
 #     # inpaint box
-#     python run_qualitative.py --config /content/InvGenPrior/configs/tmpd/afhq/inpaint_box.py --workdir /content/InvGenPrior/ --sample_N $sample_N --noise_sigma $sigma --data_index 109
+#     python run_qualitative.py --config /content/InvGenPrior/configs/tmpd/afhq/inpaint_box.py --workdir /content/InvGenPrior/ --sample_N $sample_N --noise_sigma $sigma --data_index 109 --clamp_to 1.0 --max_iter 1
 #     # inpaint pixel
-#     python run_qualitative.py --config /content/InvGenPrior/configs/tmpd/afhq/inpaint_pixel.py --workdir /content/InvGenPrior/ --sample_N $sample_N --noise_sigma $sigma --data_index 53
+#     python run_qualitative.py --config /content/InvGenPrior/configs/tmpd/afhq/inpaint_pixel.py --workdir /content/InvGenPrior/ --sample_N $sample_N --noise_sigma $sigma --data_index 53 --clamp_to 1.0 --max_iter 1
 #     # deblur
-#     python run_qualitative.py --config /content/InvGenPrior/configs/tmpd/afhq/deblur.py --workdir /content/InvGenPrior/ --sample_N $sample_N --noise_sigma $sigma --data_index 2
+#     python run_qualitative.py --config /content/InvGenPrior/configs/tmpd/afhq/deblur.py --workdir /content/InvGenPrior/ --sample_N $sample_N --noise_sigma $sigma --data_index 2 --clamp_to 1.0 --max_iter 1
 #     # super-res
-#     python run_qualitative.py --config /content/InvGenPrior/configs/tmpd/afhq/super_res.py --workdir /content/InvGenPrior/ --sample_N $sample_N --noise_sigma $sigma --data_index 15
+#     python run_qualitative.py --config /content/InvGenPrior/configs/tmpd/afhq/super_res.py --workdir /content/InvGenPrior/ --sample_N $sample_N --noise_sigma $sigma --data_index 15 --clamp_to 1.0 --max_iter 1
 # done
 
 
-# for sigma in ${sigma_list[@]}; do
-#     echo "Running qualitative evaluation for sigma = $sigma for CelebA dataset"
-#     # inpaint box
-#     python run_qualitative.py --config /content/InvGenPrior/configs/tmpd/celeba/inpaint_box.py --workdir /content/InvGenPrior/ --sample_N $sample_N --noise_sigma $sigma --data_index 109
-#     # inpaint pixel
-#     python run_qualitative.py --config /content/InvGenPrior/configs/tmpd/celeba/inpaint_pixel.py --workdir /content/InvGenPrior/ --sample_N $sample_N --noise_sigma $sigma --data_index 53
-#     # deblur
-#     python run_qualitative.py --config /content/InvGenPrior/configs/tmpd/celeba/deblur.py --workdir /content/InvGenPrior/ --sample_N $sample_N --noise_sigma $sigma --data_index 2
-#     # super-res
-#     python run_qualitative.py --config /content/InvGenPrior/configs/tmpd/celeba/super_res.py --workdir /content/InvGenPrior/ --sample_N $sample_N --noise_sigma $sigma --data_index 15
-# done
+for sigma in ${sigma_list[@]}; do
+    echo "Running qualitative evaluation for sigma = $sigma for CelebA dataset"
+    # inpaint box
+    python run_qualitative.py --config /content/InvGenPrior/configs/tmpd/celeba/inpaint_box.py --workdir /content/InvGenPrior/ --sample_N $sample_N --noise_sigma $sigma --data_index 109 --clamp_to 1.0 --max_iter 1 --sampling_var 0.0 
+    # inpaint pixel
+    python run_qualitative.py --config /content/InvGenPrior/configs/tmpd/celeba/inpaint_pixel.py --workdir /content/InvGenPrior/ --sample_N $sample_N --noise_sigma $sigma --data_index 1253 --clamp_to 1.0 --max_iter 1 --sampling_var 0.0
+    # deblur
+    python run_qualitative.py --config /content/InvGenPrior/configs/tmpd/celeba/deblur.py --workdir /content/InvGenPrior/ --sample_N $sample_N --noise_sigma $sigma --data_index 1547 --clamp_to 1.0 --max_iter 1 --sampling_var 0.0
+    # super-res
+    python run_qualitative.py --config /content/InvGenPrior/configs/tmpd/celeba/super_res.py --workdir /content/InvGenPrior/ --sample_N $sample_N --noise_sigma $sigma --data_index 389 --clamp_to 1.0 --max_iter 1 --sampling_var 0.0
+done
