7ddf8e400c8892ff65bb337ca0e34e9456ae460a
isomorphicdude <isometricat@icloud.com>
2024-09-12 15:20:30 +0100

    add comparison

--- run_qualitative.py
+++ run_qualitative.py
@@ -35,7 +35,9 @@ from guided_samplers.registry import get_guided_sampler, __GUIDED_SAMPLERS__
 
 
 def create_and_compare(config, workdir, data_index=53, noise_sigma=0.05,
-                       sample_N=100, sampling_var=0.1, clamp_to=1.0, use_svd=False,
+                       sample_N=100, sampling_var=0.1, clamp_to=1.0,
+                       starting_time= 0.2,
+                       use_svd=False,
                        max_iter=3, compare_iter=False):
     """
     Creates a result for each method and compare them.
@@ -243,7 +245,7 @@ def create_and_compare(config, workdir, data_index=53, noise_sigma=0.05,
             return_list=False,
             method=config.sampling.use_ode_sampler,  # euler by default
             clamp_to=clamp_to,
-            starting_time=config.sampling.starting_time,
+            starting_time=start_time,
             new_noise = torch.randn_like(y_obs),
             data_name = config.data.name,
             use_svd = use_svd,
@@ -301,6 +303,8 @@ flags.DEFINE_float("sampling_var", 0.1, "Sampling variance.")
 
 flags.DEFINE_float("clamp_to", 1.0, "Clamp to value.")
 
+flags.DEFINE_float("starting_time", 0.0, "Starting time for the sampler.")
+
 flags.DEFINE_integer("max_iter", 3, "Maximum number of iterations.")
 
 flags.DEFINE_string("workdir", "InvGenPrior", "Work directory.")
@@ -349,6 +353,7 @@ def main(argv):
         clamp_to=FLAGS.clamp_to,
         use_svd=FLAGS.use_svd,
         max_iter=FLAGS.max_iter,
+        starting_time=FLAGS.starting_time,
         compare_iter=FLAGS.compare_iter
     )
 
