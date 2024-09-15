7fe9b200ae780efbae28eaee1bafcb8040c3e74a
isomorphicdude <isometricat@icloud.com>
2024-09-15 14:17:37 +0100

    better format

--- tune_pgdm.bash
+++ tune_pgdm.bash
@@ -3,11 +3,11 @@
 # Tune hyperparameters for PGDM
 # only one hyperparameter: starting_time
 # nfe_list=(20 40 60 80 100 150 200)
-# noise_levels=(0.0 0.05 0.1 1.0)
-noise_levels=(0.05)
-# starting_times=(0.0 0.1 0.2 0.3 0.4 0.5 0.6)
-starting_times=(0.0 0.1)
-max_samp=4
+noise_levels=(0.0 0.05 0.1 1.0)
+# noise_levels=(0.05)
+starting_times=(0.0 0.1 0.2 0.3 0.4 0.5 0.6)
+# starting_times=(0.0 0.1)
+max_samp=50
 
 for noise_lv in ${noise_levels[@]}; do
     echo "Tuning hyperparameters for PGDM with noise level ${noise_lv}"
