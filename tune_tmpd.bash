e94118914acf1f56dd568ba9e6a65002298ae5e2
isomorphicdude <isometricat@icloud.com>
2024-09-17 11:56:42 +0100

    add tmpd tuning

--- tune_tmpd.bash
+++ tune_tmpd.bash
@@ -8,12 +8,12 @@ gmres_max_iters=(1 2 3 5)
 max_samp=20
 
 for noise_lv in ${noise_levels[@]}; do
-    echo "Tuning hyperparameters for PGDM with noise level ${noise_lv}"
+    echo "Tuning hyperparameters for TMPD with noise level ${noise_lv}"
 
     for max_iter in ${gmres_max_iters[@]}; do
-        python run_sampling.py --config configs/tmpd_cg/afhq/deblur.py  --max_num_samples ${max_samp} --compute_recon_metrics --noise_level ${noise_lv} --gmres_max_iters ${max_iter}
-        python run_sampling.py --config configs/tmpd_gmres/afhq/deblur.py  --max_num_samples ${max_samp} --compute_recon_metrics --noise_level ${noise_lv} --gmres_max_iters ${max_iter}
-        python run_sampling.py --config configs/tmpd_cgr/afhq/deblur.py  --max_num_samples ${max_samp} --compute_recon_metrics --noise_level ${noise_lv} --gmres_max_iters ${max_iter}
+        python run_sampling.py --config configs/tmpd_cg/afhq/deblur.py  --max_num_samples ${max_samp} --compute_recon_metrics --noise_level ${noise_lv} --gmres_max_iter ${max_iter}
+        python run_sampling.py --config configs/tmpd_gmres/afhq/deblur.py  --max_num_samples ${max_samp} --compute_recon_metrics --noise_level ${noise_lv} --gmres_max_iter ${max_iter}
+        python run_sampling.py --config configs/tmpd_cgr/afhq/deblur.py  --max_num_samples ${max_samp} --compute_recon_metrics --noise_level ${noise_lv} --gmres_max_iter ${max_iter}
     done
 done
 
