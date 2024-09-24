d5942874a8ebdc77a4a21298a8ad8aa87ff77e1e
isomorphicdude <isometricat@icloud.com>
2024-09-24 14:31:51 +0100

    add for learnt var

--- guided_samplers/smcdiffopt.py
+++ guided_samplers/smcdiffopt.py
@@ -286,9 +286,8 @@ class SMCDiffOpt(GuidedSampler):
                     )  # (batch * num_particles, 3, 256, 256)
                 else:
                     eps_pred = model_fn(x_t.view(model_input_shape), vec_t)
-                    print(eps_pred.shape)
-                    if eps_pred.shape[1] == 2 * x_t.shape[1]:
-                        eps_pred, model_var_values = torch.split(eps_pred, x_t.shape[1], dim=1)
+                    if eps_pred.shape[1] == 2 * self.shape[1]:
+                        eps_pred, model_var_values = torch.split(eps_pred, self.shape[1], dim=1)
 
                 x_new, x_mean_new = self.proposal_X_t(
                     num_t, x_t.view(model_input_shape), eps_pred
